fe65ea308ac76cf47d11d4c02e50d92dd6476e69
Dmitry Lyzo <ashephard0@gmail.com>
2022-01-31 20:37:26 +0300

    refactor: Use size_t and float in frame buffer

--- src/SubtitleOctopus.cpp
+++ src/SubtitleOctopus.cpp
@@ -20,12 +20,12 @@
 int log_level = 3;
 
 typedef struct {
-    void *buffer;
-    int size;
-    int lessen_counter;
+    float *buffer;
+    size_t size;
+    size_t lessen_counter;
 } buffer_t;
 
-void* buffer_resize(buffer_t *buf, int new_size, int keep_content) {
+float* buffer_resize(buffer_t *buf, size_t new_size, int keep_content) {
     if (buf->size >= new_size) {
         if (buf->size >= 1.3 * new_size) {
             // big reduction request
@@ -39,11 +39,11 @@ void* buffer_resize(buffer_t *buf, int new_size, int keep_content) {
         }
     }
 
-    void *newbuf;
+    float *newbuf;
     if (keep_content) {
-        newbuf = realloc(buf->buffer, new_size);
+        newbuf = (float*)realloc(buf->buffer, new_size);
     } else {
-        newbuf = malloc(new_size);
+        newbuf = (float*)malloc(new_size);
     }
     if (!newbuf) return NULL;
 
@@ -56,7 +56,7 @@ void* buffer_resize(buffer_t *buf, int new_size, int keep_content) {
 
 void buffer_init(buffer_t *buf) {
     buf->buffer = NULL;
-    buf->size = -1;
+    buf->size = 0;
     buf->lessen_counter = 0;
 }
 
@@ -264,7 +264,7 @@ public:
         }
 
         // make float buffer for blending
-        float* buf = (float*)buffer_resize(&m_blend, sizeof(float) * width * height * 4, 0);
+        float* buf = buffer_resize(&m_blend, sizeof(float) * width * height * 4, 0);
         if (buf == NULL) {
             printf("libass: error: cannot allocate buffer for blending");
             return &m_blendResult;
